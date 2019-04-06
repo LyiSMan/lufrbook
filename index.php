04e8966c89c0aa234ca8ea20d41fb52a3f974b86
LyiSMan <47972540+LyiSMan@users.noreply.github.com>
2019-04-06 22:41:46 +0200

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,9 @@
 	$file = fopen( $filename, "r" );
 	while (!feof($file)) {
     		$name = trim(fgets($file));
+		if(strlen($name)>0){
 		echo $name;
+		}
 	}
 	
 	//$file = fopen( $filename, "a" );
